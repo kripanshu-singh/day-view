36c1f15e173347efe00330cbc183423cafa943e1
kripanshu-singh <kripanshusingh16@gmail.com>
2025-01-29 23:34:34 +0530

    add styled-components and create initial styles for calendar and events

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import "./App.css";
+import React from "react";
+import Calendar from "./components/Calendar";
+import "./index.css";
 
-function App() {
+const events = [
+  { title: "A item start = 10, end = 90", start: 10, end: 90 },
+  { title: "B item start = 20, end = 80", start: 20, end: 80 },
+  { title: "C item start = 70, end = 180", start: 70, end: 180 },
+  { title: "D item start = 90, end = 180", start: 90, end: 180 },
+  { title: "E item start = 200, end = 270", start: 200, end: 270 },
+  { title: "F item start = 230, end = 290", start: 230, end: 290 },
+  { title: "G item start = 300, end = 340", start: 300, end: 340 },
+  { title: "Sample item start = 350, end = 400", start: 350, end: 400 },
+  { title: "Sample item", start: 370, end: 580 },
+  { title: "Sample item", start: 410, end: 480 },
+  { title: "Sample item", start: 450, end: 590 },
+  { title: "Sample item", start: 500, end: 595 },
+  { title: "Sample item", start: 530, end: 590 },
+  { title: "Sample item", start: 600, end: 660 },
+  { title: "Sample item", start: 650, end: 690 },
+  { title: "Sample item", start: 670, end: 710 },
+];
+
+const App: React.FC = () => {
   return (
-    <>
-      <h1 className="text-3xl text-red-600">App</h1>
-    </>
+    <div className="App">
+      <Calendar events={events} />
+    </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Event.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+
+const EventContainer = styled.div<{
+  top: number;
+  height: number;
+  left: number;
+  width: number;
+}>`
+  position: absolute;
+  top: ${(props) => props.top}px;
+  height: ${(props) => props.height}px;
+  left: ${(props) => props.left}%;
+  width: ${(props) => props.width}%;
+  background-color: #4a90e2;
+  color: white;
+  padding: 5px;
+  box-sizing: border-box;
+  border-radius: 4px;
+  border: 1px solid #000;
+`;
+
+interface EventProps {
+  title: string;
+  top: number;
+  height: number;
+  left: number;
+  width: number;
+}
+
+const Event: React.FC<EventProps> = ({ title, top, height, left, width }) => {
+  return (
+    <EventContainer top={top} height={height} left={left} width={width}>
+      {title}
+    </EventContainer>
+  );
+};
+
+export default Event;
